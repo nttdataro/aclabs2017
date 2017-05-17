ad11765246d3a3e04b16c72e0c3bdff9bc6d144d
ioanavaida23 <ioanavaida23@gmail.com>
2017-05-18 01:23:04 +0300

    Initial structure

---
+++ sample/src/main/java/ro/nttdata/ligaaclabs/sample/web/rest/RESTConfiguration.java
@@ -0,0 +1,24 @@
+package ro.nttdata.ligaaclabs.sample.web.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * RESTConfiguration
+ * <p>
+ * This class provides the application path that serves as the base URI for all
+ * resources.
+ *
+ */
+@ApplicationPath("/api")
+public class RESTConfiguration extends Application {
+
+	/**
+	 * Empty default constructor.
+	 * 
+	 */
+	public RESTConfiguration() {
+		super();
+	}
+
+}
